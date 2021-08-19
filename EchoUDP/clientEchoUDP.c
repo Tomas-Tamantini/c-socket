1ccbcbd3df91915072ac48b9c7725c9d7ece8041
Tomas Tamantini <tomasbft@gmail.com>
2021-08-19 10:14:04 -0300

    Measure time it takes to run

--- EchoUDP/clientEchoUDP.c
+++ EchoUDP/clientEchoUDP.c
@@ -7,6 +7,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <time.h>
 
 #define PORT 54321
 #define MAXLINE 1024
@@ -18,12 +19,60 @@ void generateMessage(char *msg, int len)
         msg[i] = 'A';
 }
 
+void runExperiment(int sockfd, struct sockaddr_in servaddr, int msgLen, int numIterations)
+{
+    char buf_from_server[MAXLINE];
+    int n;
+    for (int i = 0; i < numIterations; i++)
+    {
+        char msg[msgLen];
+        generateMessage(msg, msgLen);
+
+        sendto(sockfd, msg, msgLen,
+               MSG_CONFIRM, (const struct sockaddr *)&servaddr,
+               sizeof(servaddr));
+
+        n = recvfrom(sockfd, buf_from_server, MAXLINE,
+                     MSG_WAITALL, (struct sockaddr *)&servaddr,
+                     &n);
+
+        if (numIterations < 10)
+        {
+            // Make sure message is being received
+            printf("\nMessage echoed back from server:\n\t");
+            fputs(buf_from_server, stdout);
+        }
+        else
+        {
+            //Print progress
+            if (i % 10000 == 0)
+            {
+                int current = 10 + i / 1000;
+                int total = numIterations / 1000;
+                printf("%dk/%dk\n", current, total);
+            }
+        }
+    }
+}
+
+void runAndTimeExperiments(int sockfd, struct sockaddr_in servaddr)
+{
+    int numIterations = 100000;
+    int msgLen = 10;
+    clock_t start = clock();
+    runExperiment(sockfd, servaddr, msgLen, numIterations);
+    clock_t end = clock();
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("\nTime taken: %fs", time_taken);
+    return 0;
+}
+
 // Driver code
 int main()
 {
     int sockfd;
     char buffer[MAXLINE];
-    char buf_from_server[MAXLINE];
+
     struct sockaddr_in servaddr;
 
     // Creating socket file descriptor
@@ -40,25 +89,5 @@ int main()
     servaddr.sin_port = htons(PORT);
     servaddr.sin_addr.s_addr = INADDR_ANY;
 
-    int n;
-    int count = 0;
-    int len = 10;
-    while (count < 3)
-    {
-        count++;
-        char msg[len];
-        generateMessage(msg, len);
-
-        sendto(sockfd, msg, len,
-               MSG_CONFIRM, (const struct sockaddr *)&servaddr,
-               sizeof(servaddr));
-
-        n = recvfrom(sockfd, buf_from_server, MAXLINE,
-                     MSG_WAITALL, (struct sockaddr *)&servaddr,
-                     &n);
-        printf("\nMessage from server:\n\t");
-        buf_from_server[n] = '\0';
-        fputs(buf_from_server, stdout);
-    }
-    return 0;
+    runAndTimeExperiments(sockfd, servaddr);
 }
